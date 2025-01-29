c22df0859e2889d4cfb3210850d0224e30522d51
Jasternaut <patrushev2006@list.ru>
2025-01-29 15:23:16 +0200

    [Add] Добавлены начальные файлы и код проекта.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// Лабораторная работа 9. Интерпретация целых чисел.
+
+// Дано целое число типа unsigned short.
+// Вывести на экран содержимое каждого из его байтов,
+// используя знания по указателям.
+// Дать обоснование полученному результату.
+
+int main()
+{
+    std::cout << "[i] Starting Laboratory9..." << std::endl;
+
+    
+
+    std::cout << "[i] Laboratory9 ended succesfully!" << std::endl;
+    return 0;
+}
